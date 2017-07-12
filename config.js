ed4ced08b95804e65a2da7b1b456429848030c8f
sloanstewart <sloan_stewart@hotmail.com>
2017-07-12 18:28:17 -0400

    config updated

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app-travis';
+                      'mongodb://sloan:test123@ds155192.mlab.com:55192/blog-app-travis';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL ||
                        global.TEST_DATABASE_URL ||
                       'mongodb://localhost/blog-app-test';
